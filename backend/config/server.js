d02bbf5770ad3368efdc51253e17baef35412b3e
Higor Oliveira <46327961+higorpo@users.noreply.github.com>
2021-02-22 23:26:52 +0000

    :construction: Cria backend usando Strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'baf2afd63ff578ec2137f7ca1e762692'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c4ffa79d-0381-439f-ba39-55fc6a776eef'
+};
